8b82ee557512921b6761626fa8ef9115ffe770bd
Saulo de Carvalho Neto <saulocn@gmail.com>
2020-01-25 19:27:19 -0300

    Adicionando o Hystrix ao projeto

--- projetos/loja/src/main/java/br/com/microservice/loja/service/CompraService.java
+++ projetos/loja/src/main/java/br/com/microservice/loja/service/CompraService.java
@@ -1,10 +1,11 @@
 package br.com.microservice.loja.service;
 
-import org.bouncycastle.est.ESTAuth;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.microservice.loja.controller.dto.CompraDto;
 import br.com.microservice.loja.controller.dto.InfoFornecedorDto;
@@ -20,6 +21,7 @@ public class CompraService {
 	@Autowired
 	private FornecedorClient fornecedorClient;
 	
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
 	public Compra realizaCompra(CompraDto compra) {
 		LOG.info("Buscando informações do fornecedor de {}", compra.getEndereco().getEstado());
 		InfoFornecedorDto info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
@@ -27,7 +29,18 @@ public class CompraService {
 		System.out.println(info.getEndereco());
 		LOG.info("Realizando o Pedido");		
 		Compra compraSalva = new Compra(pedido.getId(), pedido.getTempoDePreparo(), compra.getEndereco().toString());
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return compraSalva;
+	}
+	
+	public Compra realizaCompraFallback(CompraDto compra) {
+		Compra compraFallback = new Compra();
+		compraFallback.setEndereco(compra.getEndereco().toString());
+		return compraFallback;
 	}
 
 }
